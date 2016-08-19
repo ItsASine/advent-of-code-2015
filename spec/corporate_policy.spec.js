5b1235204b4f5c1844173bad7ef24924334ee8f8
ItsASine <ItsASine@users.noreply.github.com>
2016-08-18 21:17:37 -0400

    Add day 11 tests :christmas_tree:

--- spec/corporate_policy.spec.js
+++ spec/corporate_policy.spec.js
@@ -16,4 +16,90 @@ describe('Corporate Policy', function() {
       expect(answer).toBe('heqaabcc');
     });
   });
+
+  describe('hasBadChars Function', function() {
+    it('hijklmmn', function() {
+      var input = Array.from('hijklmmn');
+      var characters = [];
+
+      input.forEach(function(char) {
+        characters.push(char.charCodeAt(0));
+      });
+
+      expect(CorporatePolicy.hasBadChars(characters)).toBe(true);
+    });
+
+    it('abbceffg', function() {
+      var input = Array.from('abbceffg');
+      var characters = [];
+
+      input.forEach(function(char) {
+        characters.push(char.charCodeAt(0));
+      });
+
+      expect(CorporatePolicy.hasBadChars(characters)).toBe(false);
+    });
+
+    it('abbcegjk', function() {
+      var input = Array.from('abbcegjk');
+      var characters = [];
+
+      input.forEach(function(char) {
+        characters.push(char.charCodeAt(0));
+      });
+
+      expect(CorporatePolicy.hasBadChars(characters)).toBe(false);
+    });
+  });
+
+  describe('fancyRules Function', function() {
+    it('hijklmmn', function() {
+      var input = Array.from('hijklmmn');
+      var characters = [];
+
+      input.forEach(function(char) {
+        characters.push(char.charCodeAt(0));
+      });
+
+      expect(CorporatePolicy.fancyRules(characters)).toBe(false);
+    });
+
+    it('abbceffg', function() {
+      var input = Array.from('abbceffg');
+      var characters = [];
+
+      input.forEach(function(char) {
+        characters.push(char.charCodeAt(0));
+      });
+
+      expect(CorporatePolicy.fancyRules(characters)).toBe(false);
+    });
+
+    it('abbcegjk', function() {
+      var input = Array.from('abbcegjk');
+      var characters = [];
+
+      input.forEach(function(char) {
+        characters.push(char.charCodeAt(0));
+      });
+
+      expect(CorporatePolicy.fancyRules(characters)).toBe(false);
+    });
+  });
+
+  describe('Password Examples', function() {
+    it('abcdefgh', function() {
+      var input = 'abcdefgh';
+      var answer = CorporatePolicy.day11(input);
+
+      expect(answer).toBe('abcdffaa');
+    });
+
+    it('ghijklmn', function() {
+      var input = 'ghijklmn';
+      var answer = CorporatePolicy.day11(input);
+
+      expect(answer).toBe('ghjaabcc');
+    })
+  });
 });
